6d928c372ebee2583864c69e75919b59b0672d47
Max Rice <max@maxrice.com>
2015-05-05 17:50:03 -0400

    Update example

--- example/example.php
+++ example/example.php
@@ -1,77 +1,105 @@
 <?php
 
-error_reporting( E_ALL );
-ini_set( 'display_errors', 'On' );
-require_once "../class-wc-api-client.php";
+require_once( 'lib/woocommerce-api.php' );
 
-$consumer_key = 'ck_fcedaba8f0fcb0fb4ae4f1211a75da72'; // Add your own Consumer Key here
-$consumer_secret = 'cs_9914968ae9adafd3741c818bf6d704c7'; // Add your own Consumer Secret here
-$store_url = 'http://localhost/demo/'; // Add the home URL to the store you want to connect to here
+$options = array(
+	'verbose_mode'    => true,
+	'return_as_array' => false,
+	'validate_url'    => false,
+	'timeout'         => 30,
+	'ssl_verify'      => false,
+);
 
-// Initialize the class
-$wc_api = new WC_API_Client( $consumer_key, $consumer_secret, $store_url );
+try {
 
-// Get Index
-//print_r( $wc_api->get_index() );
+	$client = new WC_API_Client( 'http://your-store-url.com', 'ck_enter_your_consumer_key', 'cs_enter_your_consumer_secret', $options );
 
-// Get all orders
-//print_r( $wc_api->get_orders( array( 'status' => 'completed' ) ) );
+	// coupons
+	//print_r( $client->coupons->get() );
+	//print_r( $client->coupons->get( $coupon_id ) );
+	//print_r( $client->coupons->get_by_code( 'coupon-code' ) );
+	//print_r( $client->coupons->create( array( 'code' => 'test-coupon', 'type' => 'fixed_cart', 'amount' => 10 ) ) );
+	//print_r( $client->coupons->update( $coupon_id, array( 'description' => 'new description' ) ) );
+	//print_r( $client->coupons->delete( $coupon_id ) );
+	//print_r( $client->coupons->get_count() );
 
-// Get a single order by id
-//print_r( $wc_api->get_order( 166 ) );
+	// custom
+	//$client->custom->setup( 'webhooks', 'webhook' );
+	//print_r( $client->custom->get( $params ) );
 
-// Get orders count
-//print_r( $wc_api->get_orders_count() );
+	// customers
+	//print_r( $client->customers->get() );
+	//print_r( $client->customers->get( $customer_id ) );
+	//print_r( $client->customers->get_by_email( 'help@woothemes.com' ) );
+	//print_r( $client->customers->create( array( 'email' => 'woothemes@mailinator.com' ) ) );
+	//print_r( $client->customers->update( $customer_id, array( 'first_name' => 'John', 'last_name' => 'Galt' ) ) );
+	//print_r( $client->customers->delete( $customer_id ) );
+	//print_r( $client->customers->get_count( array( 'filter[limit]' => '-1' ) ) );
+	//print_r( $client->customers->get_orders( $customer_id ) );
+	//print_r( $client->customers->get_downloads( $customer_id ) );
+	//$customer = $client->customers->get( $customer_id );
+	//$customer->customer->last_name = 'New Last Name';
+	//print_r( $client->customers->update( $customer_id, (array) $customer ) );
 
-// Get order notes for a specific order
-//print_r( $wc_api->get_order_notes( 166 ) );
+	// index
+	//print_r( $client->index->get() );
 
-// Update order status
-//print_r( $wc_api->update_order( 166, $data = array( 'status' => 'failed' ) ) );
+	// orders
+	//print_r( $client->orders->get() );
+	//print_r( $client->orders->get( $order_id ) );
+	//print_r( $client->orders->update_status( $order_id, 'pending' ) );
 
-// Get all coupons
-//print_r( $wc_api->get_coupons() );
+	// order notes
+	//print_r( $client->order_notes->get( $order_id ) );
+	//print_r( $client->order_notes->create( $order_id, array( 'note' => 'Some order note' ) ) );
+	//print_r( $client->order_notes->update( $order_id, $note_id, array( 'note' => 'An updated order note' ) ) );
+	//print_r( $client->order_notes->delete( $order_id, $note_id ) );
 
-// Get coupon by id
-//print_r( $wc_api->get_coupon( 173 ) );
+	// order refunds
+	//print_r( $client->order_refunds->get( $order_id ) );
+	//print_r( $client->order_refunds->get( $order_id, $refund_id ) );
+	//print_r( $client->order_refunds->create( $order_id, array( 'amount' => 1.00, 'reason' => 'cancellation' ) ) );
+	//print_r( $client->order_refunds->update( $order_id, $refund_id, array( 'reason' => 'who knows' ) ) );
+	//print_r( $client->order_refunds->delete( $order_id, $refund_id ) );
 
-// Get coupon by code
-//print_r( $wc_api->get_coupon_by_code( 'test coupon' ) );
+	// products
+	//print_r( $client->products->get() );
+	//print_r( $client->products->get( $product_id ) );
+	//print_r( $client->products->get( $variation_id ) );
+	//print_r( $client->products->get_by_sku( 'a-product-sku' ) );
+	//print_r( $client->products->create( array( 'title' => 'Test Product', 'type' => 'simple', 'regular_price' => '9.99', 'description' => 'test' ) ) );
+	//print_r( $client->products->update( $product_id, array( 'title' => 'Yet another test product' ) ) );
+	//print_r( $client->products->delete( $product_id, true ) );
+	//print_r( $client->products->get_count() );
+	//print_r( $client->products->get_count( array( 'type' => 'simple' ) ) );
+	//print_r( $client->products->get_categories() );
+	//print_r( $client->products->get_categories( $category_id ) );
 
-// Get coupons count
-//print_r( $wc_api->get_coupons_count() );
+	// reports
+	//print_r( $client->reports->get() );
+	//print_r( $client->reports->get_sales( array( 'filter[date_min]' => '2014-07-01' ) ) );
+	//print_r( $client->reports->get_top_sellers( array( 'filter[date_min]' => '2014-07-01' ) ) );
 
-// Get customers
-//print_r( $wc_api->get_customers() );
+	// webhooks
+	//print_r( $client->webhooks->get() );
+	//print_r( $client->webhooks->create( array( 'topic' => 'coupon.created', 'delivery_url' => 'http://requestb.in/' ) ) );
+	//print_r( $client->webhooks->update( $webhook_id, array( 'secret' => 'some_secret' ) ) );
+	//print_r( $client->webhooks->delete( $webhook_id ) );
+	//print_r( $client->webhooks->get_count() );
+	//print_r( $client->webhooks->get_deliveries( $webhook_id ) );
+	//print_r( $client->webhooks->get_delivery( $webhook_id, $delivery_id );
 
-// Get customer by id
-//print_r( $wc_api->get_customer( 2 ) );
+	// trigger an error
+	//print_r( $client->orders->get( 0 ) );
 
-// Get customer count
-//print_r( $wc_api->get_customers_count() );
+} catch ( WC_API_Client_Exception $e ) {
 
-// Get customer orders
-//print_r( $wc_api->get_customer_orders( 2 ) );
+	echo $e->getMessage() . PHP_EOL;
+	echo $e->getCode() . PHP_EOL;
 
-// Get all products
-//print_r( $wc_api->get_products() );
+	if ( $e instanceof WC_API_Client_HTTP_Exception ) {
 
-// Get a single product by id
-//print_r( $wc_api->get_product( 167 ) );
-
-// Get products count
-//print_r( $wc_api->get_products_count() );
-
-// Get product reviews
-//print_r( $wc_api->get_product_reviews( 167 ) );
-
-// Get reports
-//print_r( $wc_api->get_reports() );
-
-// Get sales report
-//print_r( $wc_api->get_sales_report() );
-
-// Get top sellers report
-// print_r( $wc_api->get_top_sellers_report() );
-
-
+		print_r( $e->get_request() );
+		print_r( $e->get_response() );
+	}
+}
